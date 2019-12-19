f537d6affeecdd18ad5fdc8f21fa7a3b327cad86
David Jennings <dkj.jennings@gmail.com>
2019-12-18 18:42:15 -0800

    more work on test.c

--- test.c
+++ test.c
@@ -3,10 +3,15 @@
 int main()
 {
     float i;
-    scanf("%.2f", &i);
-    int quarters = (i / .25);
-    i = (i - (quarters * .25));
-    printf("i = %.2f", i);
-    int dimes = (i / .1);
-    printf("%d %d", quarters, dimes);
+    scanf("%f", &i);
+    int y = i * 100;
+    int quarters = (y / 25);
+    int leftover = (y % 25);
+    printf("%d quarters\n", quarters);
+    printf("%d leftover", leftover);
+
+    // i = (i - (quarters * .25));
+    // printf("i = %f", i);
+    // float dimes = (i / .1);
+    // printf("%d, %d", quarters, dimes);
 }
