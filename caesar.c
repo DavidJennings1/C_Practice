db61ff3fd049bdcab8ea49a7ea08a55adc59ec2c
David Jennings <dkj.jennings@gmail.com>
2019-12-25 07:56:16 -0800

    Started caesar

--- caesar.c
+++ caesar.c
@@ -1,23 +1,39 @@
 // Ciphertext generator using c(i) = (p(i) + k) % 26
 
-#include <cs50.h>
+// #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, string argv[])
+int checkInput(char* key);
+
+int main(int argc, char* argv[])
 {
-    if (argc == 2)
+    if (argc != 2)
     {
-        printf("Hello, %s", argv[1]);
+        printf("Usage: ./caesar key");
+        return 1;
     }
-    else if (argc > 2)
+    printf("Success \n");
+    printf("%s\n", argv[1]);
+    printf("Return value: %i\n", checkInput(argv[1]));
+    return 0;
+}
+
+int checkInput(char* key)
+{
+    for (int i = 0, n = strlen(key); i < n; i++)
     {
-        printf("Hello, %s %i", argv[1], argv[2]);
+        if (key[i] < 48 || key[i] > 57)
+        {
+            printf("not a number\n");
+            break;
+        }
+        else
+        {
+            int idx = atoi(key);
+            return idx;
+        }
     }
-    else
-    {
-        printf("error");
-    }
-    
 }
