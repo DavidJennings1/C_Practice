85a5a3d6b561d1efa6cc7403712ef2f6bee05be0
David Jennings <dkj.jennings@gmail.com>
2019-12-25 07:57:51 -0800

    added multiple files

---
+++ string_iter.c
@@ -0,0 +1,17 @@
+/* Iterate through string input on command line
+until nul character */;
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j] != '\0'; j++)
+        {
+            printf("%c\n", argv[i][j]);
+        }
+        printf("\n");
+    }
+}
