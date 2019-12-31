61dc10aea1bbdb070e9b4e013c77a86f672f74dc
David Jennings <dkj.jennings@gmail.com>
2019-12-30 18:50:27 -0800

    Working formula

--- index_character.c
+++ index_character.c
@@ -8,23 +8,24 @@ int main(int argc, char* argv[])
     printf("input: %s\n", argv[1]);
     char oldChar;
     char newChar;
-    int key = 27;
+    int key = 1;
     int n = strlen(argv[1]);
     for (int i = 0; i < n; i++)
     {
         oldChar = argv[1][i];
-        if (oldChar >= 97 && oldChar <= 122)
+        if (oldChar >= 'a' && oldChar <= 'z')
         {
-            newChar = ((key + oldChar) % 26) + oldChar;
-            if (newChar > 122)
-                {
-                    newChar = (newChar - 122) + 96;
-                }
+            newChar = (((oldChar - 96) + key) % 26) + 96;
+            // if (newChar > 122)
+            //     {
+            //         newChar = (newChar - 122) + 96;
+            //     }
         }
         else
         {
             newChar = oldChar;
         }
+        printf("oldChar: %c\n", oldChar);
         printf("newChar: %c\n", newChar);
     }
     return 0;
