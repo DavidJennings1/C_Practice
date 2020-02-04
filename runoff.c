d2362501fb9a986dae18a764064443bf634a78d5
David Jennings <dkj.jennings@gmail.com>
2020-02-04 13:56:17 -0800

    Tabulate

--- runoff.c
+++ runoff.c
@@ -142,21 +142,24 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
+    int voter_choice;
     for (int i = 0; i < voter_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            int voter_choice = preferences[i][j];
+            voter_choice = preferences[i][j];
             if (candidates[voter_choice].eliminated)
             {
-                candidates[voter_choice].votes++;
+                voter_choice++;
             }
             else
             {
-            candidates[voter_choice].votes++;
+
             }
         }
+        candidates[voter_choice].votes++;
     }
+
     return;
 }
 
