effa23babca2624df1f74cf2b8dd4470abce4052
David Jennings <dkj.jennings@gmail.com>
2020-02-10 17:00:31 -0800

    Trying to fix winner

--- runoff.c
+++ runoff.c
@@ -162,7 +162,7 @@ bool print_winner(void)
 {
     for (int i  = 0; i < candidate_count; i++)
     {
-        float winner_test = (voter_count / 2);
+        float winner_test = ((float)voter_count / 2);
         if (candidates[i].votes > winner_test)
         {
             printf("%s\n", candidates[i].name);
