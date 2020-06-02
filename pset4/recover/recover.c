54d13a1f6f90218bfc594a0b5bcd9d3669f800eb
David Jennings <dkj.jennings@gmail.com>
2020-06-02 12:56:20 -0700

    Finds first picture

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -14,9 +14,17 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover filename");
         return 1;
     }
-    else // Prompt for input text, generate cipher output
-    {
-    }
+    BYTE test[20484];
+    FILE* file = fopen(argv[1], "r");
+    fread(test, 512, 4, file);
+    // fread(test, 512, 2, file);
+
+    printf("%x, %x, %x, %x\n", test[0], test[1], test[2], test[3]);
+    printf("%x, %x, %x, %x\n", test[511], test[512], test[513], test[514]);
+    printf("%x, %x, %x, %x\n", test[1024], test[1025], test[1026], test[1027]);
+
+
+    fclose(file);
 
 }
 // Test key input for validity
@@ -24,25 +32,25 @@ int checkInput(char* key)
 {
     printf("%s\n", key);
     int n = strlen(key);
-    FILE* file = fopen(key, "w");
+    FILE* file = fopen(key, "r");
     if (file == NULL)
     {
         printf("Fail");
         return 1;
     }
-    for (int i = 0; i < 10; i++)
-    {
-        fprintf(file, "hello\n");
-    }
-    // Need to close file before re-opening for reading
-    fclose(file);
-    // Can't re-open using same pointer name?
-    FILE* file1 = fopen(key, "r");
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     fprintf(file, "hello\n");
+    // }
+    // // Need to close file before re-opening for reading
+    // fclose(file);
+    // // Can't re-open using same pointer name?
+    // FILE* file1 = fopen(key, "r");
 
-    char ch;
-    while((ch = fgetc(file1)) != EOF)
-    {
-        printf("%c\n", ch);
-    }
-    fclose(file1);
+    // char ch;
+    // while((ch = fgetc(file1)) != EOF)
+    // {
+    //     printf("%c\n", ch);
+    // }
+    // fclose(file1);
 }
