05d3f9d32d27dad29e370550305bd62358b4ccf7
David Jennings <dkj.jennings@gmail.com>
2020-06-18 13:24:26 -0700

    Added while loop

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -6,7 +6,8 @@
 typedef uint8_t BYTE;
 
 int checkInput(char* key);
-int readBlock(BYTE* buffer, char* key);
+int readBlock(BYTE* buffer, int blockCount, char* key);
+int writeBlock(BYTE* buffer);
 
 
 int main(int argc, char *argv[])
@@ -17,26 +18,40 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover filename");
         return 1;
     }
+    int blockCount = 0;
+
     BYTE* buffer = malloc(512);
-    readBlock(buffer, argv[1]);
-    if (buffer[0] == 0 & buffer[1] == 0xd8 & buffer[2] == 0 & (buffer[3] >= 0 & buffer[3]<= 0))
+    int is_jpg = readBlock(buffer, blockCount, argv[1]);
+
+
+    while(1)
     {
-        printf("yes\n");
-    }
-    else
+    if (is_jpg == 1)
+
     {
-        {
-            printf("no\n");
-        }
-    }
         for (int i = 0; i < 512; i++)
     {
-        printf("%d\n", buffer[i]);
+        printf("%x\n", buffer[i]);
     }
-    
+        printf("from card.raw %x, %x, %x, %x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+        writeBlock(buffer);
+        break;
+    }
 
+    else
+    {
+        blockCount++;
+        is_jpg = readBlock(buffer, blockCount, argv[1]);
+    }
+    }
+    printf("%d\n", blockCount);
 
+    FILE* file2 = fopen("pic.jpg", "r");
+    fread(buffer, 1, 512, file2);
 
+    printf("from pic.jpg %x, %x, %x, %x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+
+    fclose(file2);
 
     free(buffer);
 
@@ -55,18 +70,30 @@ int checkInput(char* key)
 }
 
     // Read block to buffer
-int readBlock(BYTE* buffer, char* key)
+int readBlock(BYTE* buffer, int blockCount, char* key)
 {
-    printf("in function\n");
+    printf("in function%d\n", blockCount);
 
     FILE* file = fopen(key, "r");
+    fseek(file, (blockCount * 512), SEEK_SET);
     fread(buffer, 1, 512, file);
-
-
-    printf("%x, %x, %x, %x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
-
     fclose(file);
 
-    
-    return 1;
+
+    if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] >= 0xe0 & buffer[3]<= 0xef))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }    
+    return 0;
+}
+
+int writeBlock(BYTE* buffer)
+{
+        FILE* test = fopen("pic.jpg", "wb");
+        fwrite(buffer, 1, 512, test);
+        fclose(test);
 }
